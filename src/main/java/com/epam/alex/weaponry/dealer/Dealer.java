aface03400f06ba7b63e128bbcefd7e3e659da11
AlexandrSerebryakov <common190516@mail.ru>
2016-10-12 14:33:19 +0600

    Create classes

--- src/main/java/com/epam/alex/weaponry/dealer/Dealer.java
+++ src/main/java/com/epam/alex/weaponry/dealer/Dealer.java
@@ -1,7 +1,9 @@
 package com.epam.alex.weaponry.dealer;
 
+import com.epam.alex.Weaponry;
+
 /**
  * Created by user on 12.10.2016.
  */
-public class Dealer {
+public class Dealer extends Weaponry {
 }
