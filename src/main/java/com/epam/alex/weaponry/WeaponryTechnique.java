fedd793a3a828a5221571ca21d9fe2d9b0d58180
AlexandrSerebryakov <common190516@mail.ru>
2016-10-14 14:34:08 +0600

    Add fields: name, purchasePrice, sellingPrice to class WeaponryTechnique

--- src/main/java/com/epam/alex/weaponry/WeaponryTechnique.java
+++ src/main/java/com/epam/alex/weaponry/WeaponryTechnique.java
@@ -6,6 +6,9 @@ import com.epam.alex.Weaponry;
  * Created by user on 12.10.2016.
  */
 public class WeaponryTechnique extends Weaponry {
+    String name;
+    float purchasePrice;
+    float sellingPrice;
     int length;
     int width;
     int height;
