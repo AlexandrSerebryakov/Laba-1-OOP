be639611752e15f392e3b3b5f5d7dd00afd67c4c
AlexandrSerebryakov <common190516@mail.ru>
2016-10-14 14:28:24 +0600

    Add fields: length, width, height, weight to class WeaponryTechnique

--- src/main/java/com/epam/alex/Weaponry.java
+++ src/main/java/com/epam/alex/Weaponry.java
@@ -4,4 +4,5 @@ package com.epam.alex;
  * Created by user on 12.10.2016.
  */
 public class Weaponry {
+    int id;
 }
--- src/main/java/com/epam/alex/weaponry/WeaponryTechnique.java
+++ src/main/java/com/epam/alex/weaponry/WeaponryTechnique.java
@@ -6,4 +6,9 @@ import com.epam.alex.Weaponry;
  * Created by user on 12.10.2016.
  */
 public class WeaponryTechnique extends Weaponry {
+    int length;
+    int width;
+    int height;
+    int weight;
+
 }
