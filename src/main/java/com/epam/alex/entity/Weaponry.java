f61dcbea5d2f8476ed5784dd84208f34f1b49d1a
AlexandrSerebryakov <common190516@mail.ru>
2016-10-16 00:50:22 +0600

    Create field "vendorCode" and move all fields to class "Weaponry"

--- src/main/java/com/epam/alex/entity/Weaponry.java
+++ src/main/java/com/epam/alex/entity/Weaponry.java
@@ -4,5 +4,15 @@ package com.epam.alex.entity;
  * Created by user on 12.10.2016.
  */
 public abstract class Weaponry extends WeaponsDelivery {
-    int id;
+    private int id;
+    private String name;
+    private String vendorCode;
+    private boolean lobby;
+    private float purchasePrice;
+    private float sellingPrice;
+    private String OriginCountry;
+    private int length;
+    private int width;
+    private int height;
+    private int weight;
 }
--- src/main/java/com/epam/alex/entity/WeaponryTechnique.java
+++ src/main/java/com/epam/alex/entity/WeaponryTechnique.java
@@ -4,14 +4,6 @@ package com.epam.alex.entity;
  * Created by user on 12.10.2016.
  */
 public class WeaponryTechnique extends Weaponry {
-    private String name;
-    private boolean lobby;
-    private float purchasePrice;
-    private float sellingPrice;
-    private String OriginCountry;
-    private int length;
-    private int width;
-    private int height;
-    private int weight;
+
 
 }
