1fe9506da8056813ff4cabd9b7d5c0de4fe11db8
AlexandrSerebryakov <common190516@mail.ru>
2016-10-16 23:09:04 +0600

    Move class "WeaponryDealer" to package "service"

--- src/main/java/com/epam/alex/main/Runner.java
+++ src/main/java/com/epam/alex/main/Runner.java
@@ -8,7 +8,7 @@ import com.epam.alex.entity.WeaponsDelivery;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Bla-bla-bla");
-        WeaponsDelivery weaponsDelivery = new WeaponsDelivery("1", "Tank", "M-123a", "true", "USD", "51200", "Syria", "200", "220", "300", "3000");
+        WeaponsDelivery weaponsDelivery = new WeaponsDelivery("1", "Tank", "M-123a", "false", "USD", "51200", "Syria", "200", "220", "300", "3000");
 
     }
 }
--- src/main/java/com/epam/alex/service/WeaponryDealer.java
+++ src/main/java/com/epam/alex/service/WeaponryDealer.java
@@ -1,4 +1,4 @@
-package com.epam.alex.entity.dealer;
+package com.epam.alex.service;
 
 import com.epam.alex.entity.Weaponry;
 
@@ -6,8 +6,6 @@ import com.epam.alex.entity.Weaponry;
  * Created by user on 12.10.2016.
  */
 public class WeaponryDealer extends Weaponry {
-    //public int trade(){
-    //return values;
-    //}
+
 
 }
