6c789b541010aa4f24a1205d7de5d014f8c8a3ab
AlexandrSerebryakov <common190516@mail.ru>
2016-10-17 01:21:13 +0600

    Add List and ArrayList<>() to class "WeaponryDealer"

--- src/main/java/com/epam/alex/service/WeaponryDealer.java
+++ src/main/java/com/epam/alex/service/WeaponryDealer.java
@@ -1,9 +1,22 @@
 package com.epam.alex.service;
 
+import com.epam.alex.entity.WeaponsDelivery;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by user on 12.10.2016.
  */
 public class WeaponryDealer {
 
+    /*WeaponsDelivery weaponsDelivery = new WeaponsDelivery();*/
+    private List<WeaponsDelivery> technique;
+    private String name;
 
+
+    public WeaponryDealer(String name) {
+        technique = new ArrayList<>();
+        this.name = name;
+    }
 }
