75c727c4a38ae7f2d0c7f0b2ed3c268507a41551
AlexandrSerebryakov <common190516@mail.ru>
2016-10-16 22:59:22 +0600

    Create constructor and create a new instance of the class

--- src/main/java/com/epam/alex/entity/WeaponsDelivery.java
+++ src/main/java/com/epam/alex/entity/WeaponsDelivery.java
@@ -4,30 +4,35 @@ package com.epam.alex.entity;
  * Created by user on 14.10.2016.
  */
 public class WeaponsDelivery {
-    private int id;
+    private int idNumber;
     private String name;
     private String vendorCode;
     private boolean lobby;
-    private float purchasePrice;
-    private float sellingPrice;
-    private String OriginCountry;
+    private String currency;
+    private int sellingPrice;
+    private String originCountry;
     private int length;
     private int width;
     private int height;
     private int weight;
 
-    public WeaponsDelivery(int id, String name, String vendorCode, boolean lobby, float purchasePrice,
-                           float sellingPrice, String originCountry, int length, int width, int height, int weight) {
-        this.id = id;
+
+
+    public WeaponsDelivery(int idNumber, String name, String vendorCode, boolean lobby, String currency,
+                           int sellingPrice, String originCountry, int length, int width, int height, int weight) {
+        this.idNumber = idNumber;
         this.name = name;
         this.vendorCode = vendorCode;
         this.lobby = lobby;
-        this.purchasePrice = purchasePrice;
+        this.currency = currency;
         this.sellingPrice = sellingPrice;
-        OriginCountry = originCountry;
+        this.originCountry = originCountry;
         this.length = length;
         this.width = width;
         this.height = height;
         this.weight = weight;
     }
+
+
+
 }
--- src/main/java/com/epam/alex/main/Runner.java
+++ src/main/java/com/epam/alex/main/Runner.java
@@ -1,10 +1,14 @@
 package com.epam.alex.main;
 
+import com.epam.alex.entity.WeaponsDelivery;
+
 /**
  * Created by user on 12.10.2016.
  */
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Bla-bla-bla");
+        WeaponsDelivery weaponsDelivery = new WeaponsDelivery("1", "Tank", "M-123a", "true", "USD", "51200", "Syria", "200", "220", "300", "3000");
+
     }
 }
