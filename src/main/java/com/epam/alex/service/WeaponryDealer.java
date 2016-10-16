59efef844df172552d388ec683f15128e0c692c5
AlexandrSerebryakov <common190516@mail.ru>
2016-10-17 02:57:13 +0600

    Edit hierarchy

--- src/main/java/com/epam/alex/service/WeaponryDealer.java
+++ src/main/java/com/epam/alex/service/WeaponryDealer.java
@@ -9,37 +9,7 @@ import java.util.List;
  */
 public class WeaponryDealer {
 
-    /*private List<WeaponsDelivery> technique;*/
-    /*private String name;*/
     private List<WeaponsDelivery> weaponsDeliveries;
 
-    /*public WeaponryDealer(String name) {
-        technique = new ArrayList<WeaponsDelivery>();
-        this.name = name;
-    }*/
 
-    /*public List<WeaponsDelivery> getWeaponsDelivery(){
-
-        List<WeaponsDelivery> weaponsDeliveries = new ArrayList<>();
-        weaponsDeliveries.addAll(technique);
-
-        return weaponsDeliveries;
-    }*/
-
-    /*public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }*/
-
-    /*public List<WeaponsDelivery> getTechnique() {
-        return technique;
-    }*/
-
-    /*List<WeaponsDelivery> technique = new ArrayList<>();
-
-
-    WeaponsDeliveries weaponsDeliveries = new WeaponsDelivery("1", "To Syria", "", "", "", "", "", "", "", "", "");*/
 }
