b405945b911cf4fd10c032f5cc27f7e00026ca84
AlexandrSerebryakov <common190516@mail.ru>
2016-10-17 04:30:19 +0600

    Edit class "Main"

--- src/main/java/com/epam/alex/main/Runner.java
+++ src/main/java/com/epam/alex/main/Runner.java
@@ -1,13 +1,14 @@
 package com.epam.alex.main;
 
+import com.epam.alex.service.WeaponryDealer;
+
 /**
  * Created by user on 12.10.2016.
  */
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Bla-bla-bla");
-        /*WeaponsDelivery weaponsDelivery = new WeaponsDelivery("1", "Tank", "M-123a", "false", "USD", "51200", "Syria",
-                "200.1", "220.2", "300.1", "3000.3");*/
+        WeaponryDealer weaponryDealer = new WeaponryDealer();
 
     }
 }
