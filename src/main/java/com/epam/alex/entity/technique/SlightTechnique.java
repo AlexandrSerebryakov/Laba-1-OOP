946e9ddddb86cada05be902cd211037d976fe806
AlexandrSerebryakov <common190516@mail.ru>
2016-10-17 00:00:20 +0600

    Delete extra classes

--- src/main/java/com/epam/alex/entity/technique/SlightTechnique.java
+++
@@ -1,9 +0,0 @@
-package com.epam.alex.entity.technique;
-
-import com.epam.alex.entity.WeaponryTechnique;
-
-/**
- * Created by user on 12.10.2016.
- */
-public class SlightTechnique extends WeaponryTechnique {
-}
