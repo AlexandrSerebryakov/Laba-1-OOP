dfbbd246416d366230e82ab3e9f48830201183c4
AlexandrSerebryakov <common190516@mail.ru>
2016-10-17 00:45:18 +0600

    Add getters in class "WeaponsDelivery"and default constructor in class "Weaponry"

--- src/main/java/com/epam/alex/entity/Weaponry.java
+++ src/main/java/com/epam/alex/entity/Weaponry.java
@@ -4,5 +4,7 @@ package com.epam.alex.entity;
  * Created by user on 12.10.2016.
  */
 public abstract class Weaponry extends WeaponsDelivery {
-
+    public Weaponry(int idNumber, String name, String vendorCode, boolean lobby, String currency, int cost, String originCountry, double length, double width, double height, double weight) {
+        super(idNumber, name, vendorCode, lobby, currency, cost, originCountry, length, width, height, weight);
+    }
 }
