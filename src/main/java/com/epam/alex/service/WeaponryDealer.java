35f93dece127f87f6948b3ee0e7afdb82c3eadb8
AlexandrSerebryakov <common190516@mail.ru>
2016-10-16 23:11:10 +0600

    Delete inheritance in class "WeaponryDealer"

--- src/main/java/com/epam/alex/service/WeaponryDealer.java
+++ src/main/java/com/epam/alex/service/WeaponryDealer.java
@@ -1,11 +1,9 @@
 package com.epam.alex.service;
 
-import com.epam.alex.entity.Weaponry;
-
 /**
  * Created by user on 12.10.2016.
  */
-public class WeaponryDealer extends Weaponry {
+public class WeaponryDealer {
 
 
 }
