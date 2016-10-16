bb2092a45cba16abab25fe869888a4b967dace17
AlexandrSerebryakov <common190516@mail.ru>
2016-10-17 04:38:53 +0600

    Edit class "HeavyMilitaryTechnique", change types of variables

--- src/main/java/com/epam/alex/entity/HeavyMilitaryTechnique.java
+++ src/main/java/com/epam/alex/entity/HeavyMilitaryTechnique.java
@@ -4,7 +4,7 @@ package com.epam.alex.entity;
  * Created by user on 12.10.2016.
  */
 public class HeavyMilitaryTechnique extends WeaponryTechnique {
-    public HeavyMilitaryTechnique(int idNumber, String name, String vendorCode, boolean lobby, String currency, int cost, String originCountry, double length, double width, double height, double weight) {
+    public HeavyMilitaryTechnique(String idNumber, String name, String vendorCode, String lobby, String currency, String cost, String originCountry, String length, String width, String height, String weight) {
         super(idNumber, name, vendorCode, lobby, currency, cost, originCountry, length, width, height, weight);
     }
 }
