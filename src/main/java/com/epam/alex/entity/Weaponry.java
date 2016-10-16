ee25386210cbe3bb0f3d34df959dc9580a5e36ee
AlexandrSerebryakov <common190516@mail.ru>
2016-10-16 22:06:17 +0600

    Move all parameters from class "Weaponry" to class "WeaponsDelivery"

--- src/main/java/com/epam/alex/entity/Weaponry.java
+++ src/main/java/com/epam/alex/entity/Weaponry.java
@@ -4,15 +4,5 @@ package com.epam.alex.entity;
  * Created by user on 12.10.2016.
  */
 public abstract class Weaponry extends WeaponsDelivery {
-    private int id;
-    private String name;
-    private String vendorCode;
-    private boolean lobby;
-    private float purchasePrice;
-    private float sellingPrice;
-    private String OriginCountry;
-    private int length;
-    private int width;
-    private int height;
-    private int weight;
+
 }
--- src/main/java/com/epam/alex/entity/WeaponsDelivery.java
+++ src/main/java/com/epam/alex/entity/WeaponsDelivery.java
@@ -4,4 +4,15 @@ package com.epam.alex.entity;
  * Created by user on 14.10.2016.
  */
 public class WeaponsDelivery {
+    private int id;
+    private String name;
+    private String vendorCode;
+    private boolean lobby;
+    private float purchasePrice;
+    private float sellingPrice;
+    private String OriginCountry;
+    private int length;
+    private int width;
+    private int height;
+    private int weight;
 }
