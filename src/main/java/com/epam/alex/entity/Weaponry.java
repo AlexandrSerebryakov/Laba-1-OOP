5c5dffd83439cd23b28140a2e4b16dce0aff668d
AlexandrSerebryakov <common190516@mail.ru>
2016-10-18 15:19:25 +0600

    Changed relationships in the hierarchy and do class "Weaponry" abstract

--- src/main/java/com/epam/alex/entity/Weaponry.java
+++ src/main/java/com/epam/alex/entity/Weaponry.java
@@ -3,80 +3,9 @@ package com.epam.alex.entity;
 /**
  * Created by user on 12.10.2016.
  */
-public  class Weaponry extends WeaponsDelivery {
-    private int idNumber;
-    private String name;
-    private String vendorCode;
-    private boolean lobby;
-    private String currency;
-    private double cost;
-    private String originCountry;
-    private double length;
-    private double width;
-    private double height;
-    private double weight;
+public abstract class Weaponry extends WeaponsDelivery {
 
-
-    public Weaponry(int idNumber, String name, String vendorCode, boolean lobby, String currency,
-                    double cost, String originCountry, double length, double width, double height, double weight) {
-        this.idNumber = idNumber;
-        this.name = name;
-        this.vendorCode = vendorCode;
-        this.lobby = lobby;
-        this.currency = currency;
-        this.cost = cost;
-        this.originCountry = originCountry;
-        this.length = length;
-        this.width = width;
-        this.height = height;
-        this.weight = weight;
+    public Weaponry(int idNumber, String name, String vendorCode, boolean lobby, String currency, double cost, String originCountry, double length, double width, double height, double weight) {
+        super(idNumber, name, vendorCode, lobby, currency, cost, originCountry, length, width, height, weight);
     }
-
-
-
-    public int getIdNumber() {
-        return idNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getVendorCode() {
-        return vendorCode;
-    }
-
-    public boolean isLobby() {
-        return lobby;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public double getCost() {
-        return cost;
-    }
-
-    public String getOriginCountry() {
-        return originCountry;
-    }
-
-    public double getLength() {
-        return length;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-
 }
--- src/main/java/com/epam/alex/entity/WeaponsDelivery.java
+++ src/main/java/com/epam/alex/entity/WeaponsDelivery.java
@@ -5,6 +5,80 @@ package com.epam.alex.entity;
  */
 public class WeaponsDelivery {
 
+    private int idNumber;
+    private String name;
+    private String vendorCode;
+    private boolean lobby;
+    private String currency;
+    private double cost;
+    private String originCountry;
+    private double length;
+    private double width;
+    private double height;
+    private double weight;
+
+
+    public WeaponsDelivery(int idNumber, String name, String vendorCode, boolean lobby, String currency,
+                    double cost, String originCountry, double length, double width, double height, double weight) {
+        this.idNumber = idNumber;
+        this.name = name;
+        this.vendorCode = vendorCode;
+        this.lobby = lobby;
+        this.currency = currency;
+        this.cost = cost;
+        this.originCountry = originCountry;
+        this.length = length;
+        this.width = width;
+        this.height = height;
+        this.weight = weight;
+    }
+
+
+
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVendorCode() {
+        return vendorCode;
+    }
+
+    public boolean isLobby() {
+        return lobby;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public String getOriginCountry() {
+        return originCountry;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
 
 
 }
