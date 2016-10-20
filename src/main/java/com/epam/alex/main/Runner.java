88f3b274fa6910b8f00ac2a2ec98a9b8ecdcc1b2
AlexandrSerebryakov <common190516@mail.ru>
2016-10-21 00:53:12 +0600

    Add output of result.

--- src/main/java/com/epam/alex/main/Runner.java
+++ src/main/java/com/epam/alex/main/Runner.java
@@ -3,12 +3,12 @@ package com.epam.alex.main;
 import com.epam.alex.service.WeaponryDealer;
 
 /**
- * Created by Alexandr Serebryakov on 12.10.2016.
+ * Created by Alexandr on 09.10.2016.
  */
 public class Runner {
     public static void main(String[] args) {
         WeaponryDealer weaponryDealer = new WeaponryDealer();
-
+        System.out.println(weaponryDealer.toString());
 
     }
 }
