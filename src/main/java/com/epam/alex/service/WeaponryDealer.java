242a21c6e9e74ceb23121d2b34361772e30a4346
AlexandrSerebryakov <common190516@mail.ru>
2016-10-20 22:57:43 +0600

    Change view of values Locale parameters in class "WeaponryDealer".

--- src/main/java/com/epam/alex/service/WeaponryDealer.java
+++ src/main/java/com/epam/alex/service/WeaponryDealer.java
@@ -6,8 +6,8 @@ import org.joda.money.Money;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 
+import static java.util.Locale.*;
 import static org.joda.money.CurrencyUnit.*;
 
 /**
@@ -20,11 +20,11 @@ public class WeaponryDealer {
         List<WeaponsDelivery> weaponsDeliveries = new ArrayList<>();
         weaponsDeliveries.addAll
                 (Arrays.asList(
-                        new WeaponsDelivery(1, "Tank", "T123", true, Money.of(USD, 120000), Locale.US, 3.24, 2.34, 7.33, 6300),
-                        new WeaponsDelivery(2, "Tank", "T002", true, Money.of(USD, 120000), Locale.FRANCE, 3.55, 2.34, 2.63, 7450),
-                        new WeaponsDelivery(3, "Helicopter", "H005", false, Money.of(GBP, 120000), Locale.CANADA, 3.7, 2.45, 4.31, 5400),
-                        new WeaponsDelivery(4, "Helicopter", "H088", true, Money.of(EUR, 120000), Locale.CHINA, 3.4, 2.87, 9.77, 2890),
-                        new WeaponsDelivery(5, "Aircraft", "A745", true, Money.of(CHF, 120000), Locale.GERMANY, 30, 3, 6.33, 12000)));
+                        new WeaponsDelivery(1, "Tank", "T123", true, Money.of(USD, 120000), US, 3.24, 2.34, 7.33, 6300),
+                        new WeaponsDelivery(2, "Tank", "T002", true, Money.of(USD, 120000), FRANCE, 3.55, 2.34, 2.63, 7450),
+                        new WeaponsDelivery(3, "Helicopter", "H005", false, Money.of(GBP, 120000), CANADA, 3.7, 2.45, 4.31, 5400),
+                        new WeaponsDelivery(4, "Helicopter", "H088", true, Money.of(EUR, 120000), CHINA, 3.4, 2.87, 9.77, 2890),
+                        new WeaponsDelivery(5, "Aircraft", "A745", true, Money.of(CHF, 120000), GERMANY, 30, 3, 6.33, 12000)));
 
 
     }
